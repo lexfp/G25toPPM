500084602c401692f3b89821424aace9c10e8a20
lexfp <lexfp@nowhere.com>
2021-03-03 08:27:21 -0500

    multimodel

--- BtnPPMMap.h
+++ BtnPPMMap.h
@@ -12,10 +12,12 @@ class BtnPPMMap
     void mapShift(long value);
     void mapSteer(long value);
     int getChannelValue(int channel);
-    void debug();
     void failsafe();
     void disArm();
+    void changeCar(int car);
     int getNumChannels();
+    void debug();
+    void debugCurrentCar();
 
   private:
     //please note that these values below can be different if the wheel is not plugged into AC, so please plug in your wheel if fine tuning values below
@@ -26,9 +28,9 @@ class BtnPPMMap
     static const uint32_t NEUTRAL_GAS_INPUT = 4294944895; //value in decimal (evt->gas) when I don't step on any pedal
     static const uint32_t MAX_GAS_INPUT = 4278233088; //value in decimal (evt->gas) when I step on the gas
     static const int SHIFT_UP_INPUT = 160; //value in decimal when shifter is up
-    static const int SHIFT_DOWN_INPUT = 144; //value in decimal when shifter is down 
+    static const int SHIFT_DOWN_INPUT = 144; //value in decimal when shifter is down
     static const long DELAY_BETWEEN_SHIFT_MILLIS = 300; //amount of time between shift so you don't accidentally go from gear 1 to 5 in one motion
-    
+
     static const int CH_THROTTLE = 0;
     static const int CH_STEERING = 1;
     static const int CH_SHIFTER = 2;
@@ -37,101 +39,46 @@ class BtnPPMMap
     static const int CH_6 = 5;
     static const int CH_7 = 6;
     static const int CH_8 = 7;
-    
+
     static const int PPM_MAX_VALUE = 2000;
     static const int PPM_MIN_VALUE = 1000;
     static const int PPM_CENTER_VALUE = ((PPM_MAX_VALUE - PPM_MIN_VALUE) / 2) + PPM_MIN_VALUE;
     static const int PPM_FAIL_SAFE_VALUE = 950;
-    int PPM_THROTTLE_MAX; 
-    int PPM_REVERSE_MAX; 
-    static const int NUM_GEARS = 4;    
+    int PPM_THROTTLE_MAX;
+    int PPM_REVERSE_MAX;
+    static const int NUM_GEARS = 4;
 
-#define CAR_DEFAULT //change the CAR_DEFAULT values or add another one here for your own needs   
+#define CAR_TOTAL 3 //total number of models we have
+#define CAR_DEFAULT 0
+#define CAR_MICRO_T 1
+#define CAR_MINI_Q5 2
+#define CAR_MICRO_TRUGGY 3
+    int CURRENT_CAR = CAR_DEFAULT;
 
-//Feel free to change the settings below for the type of feel that you want in your car
-#ifdef CAR_DEFAULT
-    static const int NUM_CHANNELS = 6; //set this to however many channels your rx supports
-    static const int PPM_THROTTLE_NEUTRAL = 1500; //50/50 (1500) or 70/30 (1300)
-    static const bool WHEEL_REVERSE_STEERING = false;
-    static const int WHEEL_LEFT_TURN_LIMIT = PPM_MIN_VALUE; //set the turn limits to avoid stressing your servos
-    static const int WHEEL_CENTER = PPM_CENTER_VALUE; //this is the steering center trim value
-    static const int WHEEL_RIGHT_TURN_LIMIT = PPM_MAX_VALUE;
-    static const int FIRST_GEAR_FORWARD_LIMIT_PERCENT = 10; 
-    static const int SECOND_GEAR_FORWARD_LIMIT_PERCENT = 30;
-    static const int THIRD_GEAR_FORWARD_LIMIT_PERCENT = 50; 
-    static const int FOURTH_GEAR_FORWARD_LIMIT_PERCENT = 100;
-    static const int FIRST_GEAR_REVERSE_LIMIT_PERCENT = 30; 
-    static const int SECOND_GEAR_REVERSE_LIMIT_PERCENT = 50;
-    static const int THIRD_GEAR_REVERSE_LIMIT_PERCENT = 70; 
-    static const int FOURTH_GEAR_REVERSE_LIMIT_PERCENT = 100;
-#endif
-#ifdef CAR_MICRO_T 
-    static const int NUM_CHANNELS = 6;
-    static const int PPM_THROTTLE_NEUTRAL = 1450; 
-    static const bool WHEEL_REVERSE_STEERING = false;
-    static const int WHEEL_LEFT_TURN_LIMIT = 1100;
-    static const int WHEEL_CENTER = 1435;
-    static const int WHEEL_RIGHT_TURN_LIMIT = 1800; 
-    static const int FIRST_GEAR_FORWARD_LIMIT_PERCENT = 18; 
-    static const int SECOND_GEAR_FORWARD_LIMIT_PERCENT = 40;
-    static const int THIRD_GEAR_FORWARD_LIMIT_PERCENT = 75; 
-    static const int FOURTH_GEAR_FORWARD_LIMIT_PERCENT = 100;
-    static const int FIRST_GEAR_REVERSE_LIMIT_PERCENT = 30; 
-    static const int SECOND_GEAR_REVERSE_LIMIT_PERCENT = 40;
-    static const int THIRD_GEAR_REVERSE_LIMIT_PERCENT = 70; 
-    static const int FOURTH_GEAR_REVERSE_LIMIT_PERCENT = 100;
-#endif
-#ifdef CAR_MINI_Q5 
-    static const int NUM_CHANNELS = 6;
-    static const int PPM_THROTTLE_NEUTRAL = 1450; 
-    static const bool WHEEL_REVERSE_STEERING = true;
-    static const int WHEEL_LEFT_TURN_LIMIT = 1000;
-    static const int WHEEL_CENTER = 1450;
-    static const int WHEEL_RIGHT_TURN_LIMIT = 2000;   
-    static const int FIRST_GEAR_FORWARD_LIMIT_PERCENT = 15; 
-    static const int SECOND_GEAR_FORWARD_LIMIT_PERCENT = 50;
-    static const int THIRD_GEAR_FORWARD_LIMIT_PERCENT = 70; 
-    static const int FOURTH_GEAR_FORWARD_LIMIT_PERCENT = 100;
-    static const int FIRST_GEAR_REVERSE_LIMIT_PERCENT = 30; 
-    static const int SECOND_GEAR_REVERSE_LIMIT_PERCENT = 50;
-    static const int THIRD_GEAR_REVERSE_LIMIT_PERCENT = 70; 
-    static const int FOURTH_GEAR_REVERSE_LIMIT_PERCENT = 100;
-#endif
-#ifdef CAR_MICRO_TRUGGY  //the following are examples values for different cars
-    static const int NUM_CHANNELS = 6;
-    static const int PPM_THROTTLE_NEUTRAL = 1470; 
-    static const bool WHEEL_REVERSE_STEERING = false;
-    static const int WHEEL_LEFT_TURN_LIMIT = 1080;
-    static const int WHEEL_CENTER = 1500;
-    static const int WHEEL_RIGHT_TURN_LIMIT = 1920; 
-    static const int FIRST_GEAR_FORWARD_LIMIT_PERCENT = 10; 
-    static const int SECOND_GEAR_FORWARD_LIMIT_PERCENT = 30;
-    static const int THIRD_GEAR_FORWARD_LIMIT_PERCENT = 50; 
-    static const int FOURTH_GEAR_FORWARD_LIMIT_PERCENT = 100;
-    static const int FIRST_GEAR_REVERSE_LIMIT_PERCENT = 30; 
-    static const int SECOND_GEAR_REVERSE_LIMIT_PERCENT = 50;
-    static const int THIRD_GEAR_REVERSE_LIMIT_PERCENT = 70; 
-    static const int FOURTH_GEAR_REVERSE_LIMIT_PERCENT = 100;
-#endif
+    int NUM_CHANNELS[CAR_TOTAL];
+    int PPM_THROTTLE_NEUTRAL[CAR_TOTAL];
+    bool WHEEL_REVERSE_STEERING[CAR_TOTAL];
+    int WHEEL_LEFT_TURN_LIMIT[CAR_TOTAL];
+    int WHEEL_CENTER[CAR_TOTAL];
+    int WHEEL_RIGHT_TURN_LIMIT[CAR_TOTAL];
+    int FIRST_GEAR_FORWARD_LIMIT_PERCENT[CAR_TOTAL];
+    int SECOND_GEAR_FORWARD_LIMIT_PERCENT[CAR_TOTAL];
+    int THIRD_GEAR_FORWARD_LIMIT_PERCENT[CAR_TOTAL];
+    int FOURTH_GEAR_FORWARD_LIMIT_PERCENT[CAR_TOTAL];
+    int FIRST_GEAR_REVERSE_LIMIT_PERCENT[CAR_TOTAL];
+    int SECOND_GEAR_REVERSE_LIMIT_PERCENT[CAR_TOTAL];
+    int THIRD_GEAR_REVERSE_LIMIT_PERCENT[CAR_TOTAL];
+    int FOURTH_GEAR_REVERSE_LIMIT_PERCENT[CAR_TOTAL];
 
-    static const int PPM_THROTTLE_FORWARD_RANGE = PPM_MAX_VALUE-PPM_THROTTLE_NEUTRAL; 
-    static const int PPM_THROTTLE_REVERSE_RANGE = PPM_THROTTLE_NEUTRAL-PPM_MIN_VALUE; 
-    static const int FIRST_GEAR_FORWARD_LIMIT = ((FIRST_GEAR_FORWARD_LIMIT_PERCENT/100.0)*PPM_THROTTLE_FORWARD_RANGE)+PPM_THROTTLE_NEUTRAL; 
-    static const int SECOND_GEAR_FORWARD_LIMIT = ((SECOND_GEAR_FORWARD_LIMIT_PERCENT/100.0)*PPM_THROTTLE_FORWARD_RANGE)+PPM_THROTTLE_NEUTRAL; 
-    static const int THIRD_GEAR_FORWARD_LIMIT = ((THIRD_GEAR_FORWARD_LIMIT_PERCENT/100.0)*PPM_THROTTLE_FORWARD_RANGE)+PPM_THROTTLE_NEUTRAL; 
-    static const int FOURTH_GEAR_FORWARD_LIMIT = ((FOURTH_GEAR_FORWARD_LIMIT_PERCENT/100.0)*PPM_THROTTLE_FORWARD_RANGE)+PPM_THROTTLE_NEUTRAL; 
-    static const int FIRST_GEAR_REVERSE_LIMIT = (PPM_THROTTLE_NEUTRAL-((FIRST_GEAR_REVERSE_LIMIT_PERCENT/100.0)*PPM_THROTTLE_REVERSE_RANGE)); 
-    static const int SECOND_GEAR_REVERSE_LIMIT = (PPM_THROTTLE_NEUTRAL-((SECOND_GEAR_REVERSE_LIMIT_PERCENT/100.0)*PPM_THROTTLE_REVERSE_RANGE)); 
-    static const int THIRD_GEAR_REVERSE_LIMIT = (PPM_THROTTLE_NEUTRAL-((THIRD_GEAR_REVERSE_LIMIT_PERCENT/100.0)*PPM_THROTTLE_REVERSE_RANGE)); 
-    static const int FOURTH_GEAR_REVERSE_LIMIT = (PPM_THROTTLE_NEUTRAL-((FOURTH_GEAR_REVERSE_LIMIT_PERCENT/100.0)*PPM_THROTTLE_REVERSE_RANGE)); 
-    int GEAR_FORWARD_LIMIT[NUM_GEARS] = {FIRST_GEAR_FORWARD_LIMIT,SECOND_GEAR_FORWARD_LIMIT,THIRD_GEAR_FORWARD_LIMIT,FOURTH_GEAR_FORWARD_LIMIT};
-    int GEAR_REVERSE_LIMIT[NUM_GEARS] = {FIRST_GEAR_REVERSE_LIMIT,SECOND_GEAR_REVERSE_LIMIT,THIRD_GEAR_REVERSE_LIMIT,FOURTH_GEAR_REVERSE_LIMIT};
+    int GEAR_FORWARD_LIMIT[CAR_TOTAL][NUM_GEARS];
+    int GEAR_REVERSE_LIMIT[CAR_TOTAL][NUM_GEARS];
 
     unsigned long lastShiftUpMillis;
     unsigned long lastShiftDownMillis;
-    
+
     int currentGear;
-    int channel[NUM_CHANNELS] = {};
+#define MAX_CHANNELS 16
+    int channel[MAX_CHANNELS] = {};
     void mapGasValue(uint32_t value);
     void mapBreakValue(uint32_t value);
     void mapChannelValue(boolean invert, int channel, uint32_t value, uint32_t minValue, uint32_t maxValue, int minPPM, int maxPPM);
--- G25toPPM.ino
+++ G25toPPM.ino
@@ -20,7 +20,9 @@
 #define FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
 #define PULSE_LENGTH 300  //set the pulse length
 #define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
-#define sigPin 3  //set PPM signal output pin on the arduino
+#define PPM_PIN 3  //set PPM signal output pin on the arduino
+#define INPUT1 6 //pin for switch 0
+#define INPUT2 7 //pin for switch 1
 
 BtnPPMMap btnPPMMap;
 
@@ -84,20 +86,18 @@ void JoystickEvents::disconnectJoystick()
 
 void JoystickEvents::OnGamePadChanged(const GamePadEventData *evt)
 {
-  
-    Serial.print("W: ");
-    Serial.print(evt->wheel, DEC);
-    Serial.print("S: ");
-    Serial.print(evt->shift, DEC);
-    Serial.print("T: ");
-    Serial.print(evt->gas, DEC);
-    Serial.println();
-  
+  Serial.print("IN - T:");
+  Serial.print(evt->gas, DEC);
+  Serial.print(" W:");
+  Serial.print(evt->wheel, DEC);
+  Serial.print(" S:");
+  Serial.print(evt->shift, DEC);
+  Serial.println();
+
   btnPPMMap->mapShift(evt->shift);
   btnPPMMap->mapGas(evt->gas);
   btnPPMMap->mapSteer(evt->wheel);
   btnPPMMap->debug();
-
 }
 
 USB                                             Usb;
@@ -123,8 +123,22 @@ void setup()
   if (!Hid.SetReportParser(0, &Joy))
     ErrorMessage<uint8_t>(PSTR("SetReportParser"), 1  );
 
-  pinMode(sigPin, OUTPUT);
-  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  pinMode(PPM_PIN, OUTPUT);
+  digitalWrite(PPM_PIN, !onState);  //set the PPM signal pin to the default state (off)
+
+  //set up switches for car selection
+  pinMode( INPUT1, INPUT_PULLUP );
+  pinMode( INPUT2, INPUT_PULLUP );
+
+  //select the car
+  if (digitalRead( INPUT1 ) == 0) {
+    btnPPMMap.changeCar(CAR_MICRO_T);
+  } else if (digitalRead( INPUT2 ) == 0) {
+    btnPPMMap.changeCar(CAR_MICRO_TRUGGY);
+  } else {
+    //default if switches are not wired
+    btnPPMMap.changeCar(CAR_MINI_Q5);
+  }
 
   cli();
   TCCR1A = 0; // set entire TCCR1 register to 0
@@ -138,7 +152,7 @@ void setup()
 }
 
 void loop()
-{
+{ 
   Usb.Task();
   if (Usb.getUsbTaskState() != USB_STATE_RUNNING)
   {
@@ -154,7 +168,7 @@ ISR(TIMER1_COMPA_vect) { //leave this alone
   TCNT1 = 0;
 
   if (state) {  //start pulse
-    digitalWrite(sigPin, onState);
+    digitalWrite(PPM_PIN, onState);
     OCR1A = PULSE_LENGTH * 2;
     state = false;
   } else { //end pulse and calculate when to start the next pulse
@@ -162,7 +176,7 @@ ISR(TIMER1_COMPA_vect) { //leave this alone
     int channelValue;
     static unsigned int calc_rest;
 
-    digitalWrite(sigPin, !onState);
+    digitalWrite(PPM_PIN, !onState);
     state = true;
 
     if (cur_chan_numb >= btnPPMMap.getNumChannels()) {
